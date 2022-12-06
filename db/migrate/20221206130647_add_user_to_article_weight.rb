51b81ffc5ce2fdef308f2bc2b4ff1543ca0d0351
Ivan Silva <ivankuartz@hotmail.com>
2022-12-06 11:04:16 -0300

    Add references to recent migrations

--- db/migrate/20221206130647_add_user_to_article_weight.rb
+++ db/migrate/20221206130647_add_user_to_article_weight.rb
@@ -1,4 +1,5 @@
 class AddUserToArticleWeight < ActiveRecord::Migration[7.0]
   def change
+    add_reference :article_weights, :user
   end
 end
--- db/migrate/20221206132136_add_user_to_article.rb
+++ db/migrate/20221206132136_add_user_to_article.rb
@@ -1,4 +1,5 @@
 class AddUserToArticle < ActiveRecord::Migration[7.0]
   def change
+    add_reference :article, :user
   end
 end
