76b1be0a17c7fd40367a1b93af7d14feeb48f040
Ivan Silva <ivankuartz@hotmail.com>
2022-12-06 17:28:22 -0300

    Linter corrections

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_144329) do
+ActiveRecord::Schema[7.0].define(version: 20_221_206_144_329) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "article_weights", force: :cascade do |t|
-    t.integer "weight"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "article_id"
-    t.index ["article_id"], name: "index_article_weights_on_article_id"
-    t.index ["user_id"], name: "index_article_weights_on_user_id"
+  create_table 'article_weights', force: :cascade do |t|
+    t.integer 'weight'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.bigint 'article_id'
+    t.index ['article_id'], name: 'index_article_weights_on_article_id'
+    t.index ['user_id'], name: 'index_article_weights_on_user_id'
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.integer "relevance"
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "theme_id"
-    t.index ["theme_id"], name: "index_articles_on_theme_id"
-    t.index ["user_id"], name: "index_articles_on_user_id"
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.integer 'relevance'
+    t.string 'text'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.bigint 'theme_id'
+    t.index ['theme_id'], name: 'index_articles_on_theme_id'
+    t.index ['user_id'], name: 'index_articles_on_user_id'
   end
 
-  create_table "theme_weights", force: :cascade do |t|
-    t.integer "weight"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "theme_id"
-    t.index ["theme_id"], name: "index_theme_weights_on_theme_id"
-    t.index ["user_id"], name: "index_theme_weights_on_user_id"
+  create_table 'theme_weights', force: :cascade do |t|
+    t.integer 'weight'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.bigint 'user_id'
+    t.bigint 'theme_id'
+    t.index ['theme_id'], name: 'index_theme_weights_on_theme_id'
+    t.index ['user_id'], name: 'index_theme_weights_on_user_id'
   end
 
-  create_table "themes", force: :cascade do |t|
-    t.string "theme"
-    t.integer "relevance"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'themes', force: :cascade do |t|
+    t.string 'theme'
+    t.integer 'relevance'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.string 'password'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "article_weights", "articles"
-  add_foreign_key "article_weights", "users"
-  add_foreign_key "articles", "themes"
-  add_foreign_key "articles", "users"
-  add_foreign_key "theme_weights", "themes"
-  add_foreign_key "theme_weights", "users"
+  add_foreign_key 'article_weights', 'articles'
+  add_foreign_key 'article_weights', 'users'
+  add_foreign_key 'articles', 'themes'
+  add_foreign_key 'articles', 'users'
+  add_foreign_key 'theme_weights', 'themes'
+  add_foreign_key 'theme_weights', 'users'
 end
