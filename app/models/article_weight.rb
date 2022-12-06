f44566e5261be859f7bdb4987c70dfd22d2471cf
Ivan Silva <ivankuartz@hotmail.com>
2022-12-05 22:34:24 -0300

    Generate article weight

---
+++ app/models/article_weight.rb
@@ -0,0 +1,2 @@
+class ArticleWeight < ApplicationRecord
+end
---
+++ test/models/article_weight_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ArticleWeightTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
