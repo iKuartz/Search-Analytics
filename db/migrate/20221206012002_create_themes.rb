b7578ca86f0d61a78757355e49c285f03ba8fa50
Ivan Silva <ivankuartz@hotmail.com>
2022-12-05 22:20:30 -0300

    Create Theme migration

---
+++ db/migrate/20221206012002_create_themes.rb
@@ -0,0 +1,10 @@
+class CreateThemes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :themes do |t|
+      t.string :theme
+      t.integer :relevance
+
+      t.timestamps
+    end
+  end
+end
