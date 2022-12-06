4f8b9cb80e49ada1ec87be0ef327c1d92534bbc8
Ivan Silva <ivankuartz@hotmail.com>
2022-12-06 12:09:09 -0300

    Add foreign key to article weight

--- db/migrate/20221206143713_add_foreign_key_to_article_weight.rb
+++ db/migrate/20221206143713_add_foreign_key_to_article_weight.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyToArticleWeight < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key :article_weight, :articles
-    add_foreign_key :article_weight, :users
+    add_foreign_key :article_weights, :articles
+    add_foreign_key :article_weights, :users
   end
 end
